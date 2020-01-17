8b18bac29fa05005b705754b3696ccf56b6bd0b8
CO\MM07860 <shuji_mikami@murata.com>
2020-01-17 20:53:12 +0900

    gccではなくgdbの間違い

--- gdb_openocd_debugger/gdb_runner.py
+++ gdb_openocd_debugger/gdb_runner.py
@@ -8,45 +8,45 @@ import sys
 import threading
 
 
-gccRunFlag = True
-def gccThread(gccProc):
+gdbRunFlag = True
+def gdbThread(gdbProc):
     
-    while gccRunFlag == True:
-        out = gccProc.stdout.read(1)
-        if out == '' and gccProc.poll() != None:
+    while gdbRunFlag == True:
+        out = gdbProc.stdout.read(1)
+        if out == '' and gdbProc.poll() != None:
             break
         if out != '':
             sys.stdout.write(out.decode())
             sys.stdout.flush()
 
 FW_WORKSPACE_ROOT = r'C:\Users\mm07860\workspace\gohei_system4\FW'
-GCC_EXECUTABLE = os.environ['ARM_NONE_EABI_TOOLS_DIR'] + r'\arm-none-eabi-gdb'
+GDB_EXECUTABLE = os.environ['ARM_NONE_EABI_TOOLS_DIR'] + r'\arm-none-eabi-gdb'
 os.chdir(FW_WORKSPACE_ROOT)
 
-gccProc = sp.Popen(GCC_EXECUTABLE + r' .\build\gohei_system4_FW.elf', stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE)
+gdbProc = sp.Popen(GDB_EXECUTABLE + r' .\build\gohei_system4_FW.elf', stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE)
 
-thread_gcc = threading.Thread(target = gccThread, args=[gccProc])
+thread_gdb = threading.Thread(target = gdbThread, args=[gdbProc])
 
-thread_gcc.start()
+thread_gdb.start()
 
 time.sleep(2)
 
-gccProc.stdin.write('target remote localhost:3333'.encode("utf8") + b"\n")
-gccProc.stdin.flush()
+gdbProc.stdin.write('target remote localhost:3333'.encode("utf8") + b"\n")
+gdbProc.stdin.flush()
 time.sleep(1)
-gccProc.stdin.write('interrupt'.encode("utf8") + b"\n")
-gccProc.stdin.flush()
+gdbProc.stdin.write('interrupt'.encode("utf8") + b"\n")
+gdbProc.stdin.flush()
 time.sleep(1)
-gccProc.stdin.write('monitor reset halt'.encode("utf8") + b"\n")
-gccProc.stdin.flush()
+gdbProc.stdin.write('monitor reset halt'.encode("utf8") + b"\n")
+gdbProc.stdin.flush()
 time.sleep(1)
-gccProc.stdin.write('load'.encode("utf8") + b"\n")
-gccProc.stdin.flush()
+gdbProc.stdin.write('load'.encode("utf8") + b"\n")
+gdbProc.stdin.flush()
 
 time.sleep(5)
 
 
-gccRunFlag = False
+gdbRunFlag = False
 
-gccProc.kill()
+gdbProc.kill()
 
